e7e809af5badb0e63d7866e498918eb549c6bc2c
Matan Zvulun <matanzv@gmail.com>
2022-02-04 13:01:57 +0200

    added splash activity

---
+++ app/src/main/java/com/example/unigrades/SplashActivity.java
@@ -0,0 +1,76 @@
+package com.example.unigrades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final String TAG = SplashActivity.class.getSimpleName();
+
+    final int ANIM_DURATION = 6000;
+
+    private ImageView imageLogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        findViews();
+
+        imageLogo.setVisibility(View.INVISIBLE);
+
+        showViewSlideDown(imageLogo);
+    }
+
+    private void findViews() {
+        imageLogo = findViewById(R.id.splash_IMAGE_logo);
+    }
+
+    private void showViewSlideDown(final View v){
+        v.setVisibility(View.VISIBLE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        v.setY(-height/2);
+        v.setScaleY(0.0f);
+        v.setScaleX(0.0f);
+        v.animate().
+                scaleX(1.0f).
+                scaleY(1.0f).
+                translationY(0).
+                setDuration(ANIM_DURATION).
+                setInterpolator(new AccelerateInterpolator()).
+                setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                animationDone();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+    }
+
+    private void animationDone() {
+        MyGlobalFunctions.startNewActivity(this,SignInActivity.class);
+    }
+}
